9f4d4c03363d45c5c6ad47d2e56150212a609ed0
doswald32 <dan.oswald32@gmail.com>
2023-12-30 16:46:59 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    let newText = document.getElementById('text');
+    newText.textContent = "This is really cool!";
+    console.log(newText);
+});
+
+console.log('Will this show up BEFORE or AFTER the DOM content loaded has triggered?');
